706713137deb697e83f3034bb6e5250ecb92ef6b
Viraj <jadhavviraj50@gmail.com>
2022-05-13 21:57:44 +0530

    [Viraj]-Added UC1-AddElementsInTree file

---
+++ BinarySearchTree/BinarySearchTree/Program.cs
@@ -0,0 +1,7 @@
+﻿using BinarySearchTree;
+
+Console.WriteLine("-------Welcome To Binary Search Program------");
+BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+binarySearch.Insert(30);
+binarySearch.Insert(70);
+binarySearch.Display();
